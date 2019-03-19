bbd15d8cd0d17b2e7eed02fffba67d030d677012
Carlos del Prado <carlos.delprado@11paths.com>
2019-03-19 23:17:33 +0100

    fix: navbar tags

--- users-web/src/components/misc/NavBar.js
+++ users-web/src/components/misc/NavBar.js
@@ -5,22 +5,22 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mt-5" style={{ borderRadius: "5px"}}>
       <Link className="navbar-brand" to="/users">Auth Context Lab</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassNameName="active" to="/users">Users</NavLink>
+            <NavLink className="nav-link" activeClassName="active" to="/users">Users</NavLink>
           </li>
         </ul>
         <ul className="navbar-nav my-2 my-lg-0">
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassNameName="active" to="/register">Register</NavLink>
+            <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" activeClassNameName="active" to="/login">Login</NavLink>
+            <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
           </li>
         </ul>
       </div>
